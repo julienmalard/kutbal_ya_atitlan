c0e98e9d01175c65fc3a5d921b41ff398ca5477f
Jessica <bounassarjessica@gmail.com>
2020-03-26 21:17:22 -0400

    Xinyaq jub'a ajlanik

--- atitlan/vensim/rubanom_wachibal.py
+++ atitlan/vensim/rubanom_wachibal.py
@@ -2,6 +2,7 @@ import xarray as xr
 import os
 from atitlan.vensim.utils import DIR_RES
 import json
+import matplotlib.pyplot as plt
 
 res = {}
 for wj in os.listdir(DIR_RES):
@@ -10,4 +11,15 @@ for wj in os.listdir(DIR_RES):
         with open(os.path.join(DIR_RES, wj), 'r', encoding='utf8') as w:
             res[rubi] = xr.DataArray.from_dict(json.load(w))
 
-input()
+DIR_WCHBL = os.path.join(DIR_RES, 'wchbl')
+os.makedirs(DIR_WCHBL, exist_ok=True)
+
+for rtl, ajl in res.items():
+    plt.plot(ajl, label=rtl)
+    # if rtl != 'Runoff' and 'Runoff' in res:
+    #     plt.plot(res['Runoff'], label='Runoff')
+    plt.title(rtl)
+    plt.legend()
+
+    plt.savefig(os.path.join(DIR_WCHBL, rtl + '.png'))
+    plt.clf()
